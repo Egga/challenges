8223b1b82bc43edeaaf934b9e39f96217bbb7252
Egga (Jan Hartung) <github@egga.de>
2015-05-28 22:28:37 +0200

    add second test and coverage reports

--- challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
+++ challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
@@ -13,14 +13,26 @@ public class TicTest {
     public static final String STUPID_INVISIBLE_CHARACTER_AT_BEGINNING_OF_STRING = "\u0000";
 
     @Test
-    public void itName() throws IOException {
+    public void test_one() throws IOException {
         UserInputService inputService = mock(UserInputService.class);
         TirageFactory tirageFactory = mock(TirageFactory.class);
         Tic tic = new Tic(inputService, tirageFactory);
         when(inputService.nextInt()).thenReturn(1, 2, 3, 4, 5, 6, 7, 8, 9);
         when(tirageFactory.getTirage()).thenReturn(0.1f);
         tic.eval();
-        String actual = String.valueOf (tic.tab);
+        String actual = String.valueOf(tic.tab);
         assertThat(actual).isEqualTo(STUPID_INVISIBLE_CHARACTER_AT_BEGINNING_OF_STRING + "xoxoxoxox");
+    }
+
+    @Test
+    public void test_two() throws IOException {
+        UserInputService inputService = mock(UserInputService.class);
+        TirageFactory tirageFactory = mock(TirageFactory.class);
+        Tic tic = new Tic(inputService, tirageFactory);
+        when(inputService.nextInt()).thenReturn(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        when(tirageFactory.getTirage()).thenReturn(0.9f);
+        tic.eval();
+        String actual = String.valueOf(tic.tab);
+        assertThat(actual).isEqualTo(STUPID_INVISIBLE_CHARACTER_AT_BEGINNING_OF_STRING + "oxoxoxoxo");
     }
 }
