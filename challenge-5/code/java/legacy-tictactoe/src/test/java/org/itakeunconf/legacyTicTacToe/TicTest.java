f3e9e870d4b09e83e85aae140fd56ac6e6240ea6
Egga (Jan Hartung) <github@egga.de>
2015-05-28 15:54:28 +0200

    test should be fixed

--- challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
+++ challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
@@ -15,8 +15,9 @@ public class TicTest {
         UserInputService inputService = mock(UserInputService.class);
         TirageFactory tirageFactory = mock(TirageFactory.class);
         Tic tic = new Tic(inputService, tirageFactory);
-        when(inputService.nextInt()).thenReturn(1, 2, 3, 4, 5);
+        when(inputService.nextInt()).thenReturn(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        when(tirageFactory.getTirage()).thenReturn(0.1f);
         tic.eval();
-        assertThat(String.valueOf(tic.tab)).isEqualTo("o");
+        assertThat(String.valueOf(tic.tab)).isEqualTo("xoxoxoxox");
     }
 }
