c50653021945e07903d1e2afba273417cb26d73d
Egga (Jan Hartung) <github@egga.de>
2015-11-06 10:47:23 +0100

    last phase

--- challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
+++ challenge-5/code/java/legacy-tictactoe/src/test/java/org/itakeunconf/legacyTicTacToe/TicTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.paukov.combinatorics.Factory;
+import org.paukov.combinatorics.Generator;
+import org.paukov.combinatorics.ICombinatoricsVector;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -25,39 +30,38 @@ public class TicTest {
 
     Tic tic;
 
-    @Before
-    public void setUp() throws Exception {
-        tic = new Tic(inputService, tirageFactory);
-    }
-
     @Test
-    public void test_one() throws IOException {
-        beginWithPlayerA();
-        makeMoves(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        tic.eval();
-        assertGameResultEquals("xoxoxoxox");
+    public void itName() {
+        ICombinatoricsVector<Integer> originalVector = Factory.createVector(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+        Generator<Integer> gen = Factory.createSimpleCombinationGenerator(originalVector, 5);
+
+        long numberOfGeneratedObjects = gen.getNumberOfGeneratedObjects();
+        System.out.println(numberOfGeneratedObjects);
+
+        Float[] floats = {0.1f, 0.9f};
+        for (Float tirage : floats) {
+            beginWithPlayerA(tirage);
+            for (ICombinatoricsVector<Integer> perm : gen) {
+                tic = new Tic(inputService, tirageFactory);
+                List<Integer> vector = perm.getVector();
+
+                List<Integer> otherPlayersMoves = new ArrayList<Integer>();
+                for (int i = 1; i < 10; i++) {
+                    if (!vector.contains(i)) {
+                        otherPlayersMoves.add(i);
+                    }
+                }
+
+                Integer first = vector.remove(0);
+                makeMoves(first, vector.toArray(new Integer[vector.size()]));
+            }
+        }
     }
 
-    private void assertGameResultEquals(String xoxoxoxox) {
-        assertThat(getGameResult()).isEqualTo(STUPID_INVISIBLE_CHARACTER_AT_BEGINNING_OF_STRING + xoxoxoxox);
+    private void assertGameResultEquals(String expected) {
+        assertThat(getGameResult()).isEqualTo(STUPID_INVISIBLE_CHARACTER_AT_BEGINNING_OF_STRING + expected);
     }
-
-    @Test
-    public void test_two() throws IOException {
-        beginWithPlayerB();
-        makeMoves(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        tic.eval();
-        assertGameResultEquals("oxoxoxoxo");
-    }
-
-    @Test
-    public void test_three() throws IOException {
-        beginWithPlayerB();
-        makeMoves(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        tic.eval();
-        assertGameResultEquals("oxoxoxoxo");
-    }
-
 
     private String getGameResult() {
         return String.valueOf(tic.tab);
@@ -67,11 +71,8 @@ public class TicTest {
         when(inputService.nextInt()).thenReturn(firstMove, moves);
     }
 
-    private void beginWithPlayerA() {
-        when(tirageFactory.getTirage()).thenReturn(0.1f);
+    private void beginWithPlayerA(Float value) {
+        when(tirageFactory.getTirage()).thenReturn(value);
     }
 
-    private void beginWithPlayerB() {
-        when(tirageFactory.getTirage()).thenReturn(0.9f);
-    }
 }
